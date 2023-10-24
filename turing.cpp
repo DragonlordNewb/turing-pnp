da5b90d7308df4407efa3255513d312919027a02
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-10-24 08:03:29 -0500

    Create turing.cpp

---
+++ turing.cpp
@@ -0,0 +1,33 @@
+using std::vector;
+
+class Tape {
+
+	private:
+
+		vector<int> tape;
+
+	public:
+
+		int currentLength() {
+			return self.
+		}
+
+		void ensure(int index) {
+			if 
+		}
+
+};
+
+class TuringMachine {
+
+	public:
+
+		T currentState;
+
+		TuringMachine(T state) {
+
+			currentState = state;
+			
+		}
+
+};
